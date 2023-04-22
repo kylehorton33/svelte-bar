1d437b7af381b53171d17fdf3efc769aa73cf51a
Kyle Horton <kylehorton33@gmail.com>
2023-04-22 17:28:02 -0400

    fix:

--- src/routes/ingredient/[slug]/+page.js
+++ src/routes/ingredient/[slug]/+page.js
@@ -4,7 +4,6 @@ import { getIngredientBySlug, getImageUrlBySlug } from '$lib/utils/api'
 export async function load({ params }) {
 	let ingredient = await getIngredientBySlug(params.slug);
 	let imageURL = await getImageUrlBySlug(params.slug)
-	console.log(ingredient);
 	return {
 		slug: params.slug,
 		imageURL, 
