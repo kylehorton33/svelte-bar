e1b9947bf6a3613d85b41661c6c7fe6b0a7fc43e
Kyle Horton <kylehorton33@gmail.com>
2023-04-21 20:02:29 -0400

    docs: description of API

--- src/lib/utils/api.js
+++ src/lib/utils/api.js
@@ -1,8 +1,11 @@
+// Import db, storage from firebase
 import { db, storage } from './firebase';
+
+// Import functions from SDKs
 import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import { ref, getDownloadURL } from 'firebase/storage';
 
-
+// Return all ingredients from Firestore
 export async function getAllIngredients() {
     /**
      * @type {{ slug: string; title: any; }[]}
@@ -18,6 +21,7 @@ export async function getAllIngredients() {
 /**
  * @param {string} slug
  */
+// Return single ingredient by slug
 export async function getIngredientBySlug(slug) {
     const docRef = doc(db, "ingredients", slug)
     const docSnap = await getDoc(docRef);
@@ -34,6 +38,7 @@ export async function getIngredientBySlug(slug) {
 /**
  * @param {any} slug
  */
+// Return image by slug
 export async function getImageUrlBySlug(slug) {
     const docRef = ref(storage, `images/${slug}.jpeg`)
     const URL = await getDownloadURL(docRef);
