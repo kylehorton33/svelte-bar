b3f9534c5ae3349e81dea682631d943934340649
Kyle Horton <kyle@cplventures.com>
2023-04-22 13:30:29 -0400

    feat: :lipstick: udpate search bar on ingredients page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,14 @@
 <script>
+	import Search from '$lib/components/Search.svelte';
 	import ListItem from '$lib/components/ListItem.svelte';
 	
 	export let data;
-	console.log(data.ingredients)
 </script>
 
-<div id="search-bar">
-	<input type="text" id="drink-search" placeholder="Search bar..." />
-</div>
+<Search />
 
 <ul>
 	{#each data.ingredients as {slug, name}}
 		<ListItem type="ingredient" {slug} {name} />
 	{/each}
 </ul>
-
-<style>
-	#search-bar {
-		margin: 2rem 3rem;
-		display: flex;
-	}
-	#search-bar input {
-		width: 100%;
-		height: 1.3rem;
-		border-radius: 1rem;
-	}
-</style>
